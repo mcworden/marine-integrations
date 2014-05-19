bdc6efeced811a045ed967d8c643d25c5480db57
Mark Worden <mark_c_worden@raytheon.com>
2014-05-19 10:24:43 -0700

    Integration and qual test driver for dosta_ln_wfp

--- mi/dataset/driver/dosta_ln/wfp/test/test_driver.py
+++ mi/dataset/driver/dosta_ln/wfp/test/test_driver.py
@@ -26,10 +26,13 @@ from mi.idk.exceptions import SampleTimeout
 from mi.idk.dataset.unit_test import DataSetTestCase
 from mi.idk.dataset.unit_test import DataSetIntegrationTestCase
 from mi.idk.dataset.unit_test import DataSetQualificationTestCase
+from mi.dataset.dataset_driver import DriverParameter
 
 from mi.dataset.dataset_driver import DataSourceConfigKey, DataSetDriverConfigKeys
 from mi.dataset.driver.dosta_ln.wfp.driver import DostaLnWfpDataSetDriver
-from mi.dataset.parser.dosta_ln_wfp import HEADER_BYTES, WFP_E_GLOBAL_RECOVERED_ENG_DATA_SAMPLE_BYTES
+from mi.dataset.parser.dosta_ln_wfp import HEADER_BYTES, DataParticleType, WFP_E_GLOBAL_RECOVERED_ENG_DATA_SAMPLE_BYTES
+from pyon.agent.agent import ResourceAgentState
+from interface.objects import ResourceAgentErrorEvent
 
 # Fill in driver details
 DataSetTestCase.initialize(
@@ -76,15 +79,15 @@ class IntegrationTest(DataSetIntegrationTestCase):
         self.driver.start_sampling()
 
         self.clear_async_data()
-        self.create_sample_data('first.DAT', "E0000001.DAT")
+        self.create_sample_data('E0000001.DAT', "E0000001.DAT")
         self.assert_data(None, 'first.yml', count=1, timeout=10)
 
         # Read the remaining values in first.DAT
         self.assert_data(None, None, count=682, timeout=100)
 
         self.clear_async_data()
-        self.create_sample_data('second.DAT', "E0000002.DAT")
-        self.assert_data(None, 'second.yml', count=6, timeout=10)
+        self.create_sample_data('E0000002.DAT', "E0000002.DAT")
+        self.assert_data(None, 'six_samples.yml', count=6, timeout=10)
 
         # Read the remaining values in second.DAT
         self.assert_data(None, None, count=677, timeout=100)
@@ -98,8 +101,8 @@ class IntegrationTest(DataSetIntegrationTestCase):
         """
         Test the ability to stop and restart the process
         """
-        path_1 = self.create_sample_data('first.DAT', "E0000001.DAT")
-        path_2 = self.create_sample_data('second.DAT', "E0000002.DAT")
+        path_1 = self.create_sample_data('E0000001.DAT', "E0000001.DAT")
+        path_2 = self.create_sample_data('E0000002.DAT', "E0000002.DAT")
 
         # Create and store the new driver state
         # The first file will be fully ingested
@@ -117,7 +120,7 @@ class IntegrationTest(DataSetIntegrationTestCase):
         self.driver.start_sampling()
 
         # verify data is produced
-        self.assert_data(None, 'second.partial.yml', count=2, timeout=10)
+        self.assert_data(None, 'two_samples.yml', count=2, timeout=10)
 
 
     def test_stop_start_resume(self):
@@ -129,8 +132,8 @@ class IntegrationTest(DataSetIntegrationTestCase):
 
         self.driver.start_sampling()
 
-        self.create_sample_data('first.DAT', "E0000001.DAT")
-        self.create_sample_data('second.DAT', "E0000002.DAT")
+        self.create_sample_data('E0000001.DAT', "E0000001.DAT")
+        self.create_sample_data('E0000002.DAT', "E0000002.DAT")
 
         # Read the first record and compare it
         self.assert_data(None, 'first.yml', count=1, timeout=10)
@@ -148,7 +151,7 @@ class IntegrationTest(DataSetIntegrationTestCase):
         self.driver.start_sampling()
 
         # Read 2 records and compare them
-        self.assert_data(None, 'second.partial.yml', count=2, timeout=10)
+        self.assert_data(None, 'two_samples.yml', count=2, timeout=10)
 
         # Read the remaining 677 records from E0000002.DAT
         self.assert_data(None, None, count=677, timeout=100)
@@ -187,32 +190,171 @@ class QualificationTest(DataSetQualificationTestCase):
         Setup an agent/driver/harvester/parser and verify that data is
         published out the agent
         """
-        pass
+        self.create_sample_data('small.DAT', 'E0000001.DAT')
+        # self.assert_initialize(final_state=ResourceAgentState.COMMAND)
+        #
+        # # NOTE: If the processing is not slowed down here, the engineering samples are
+        # # returned in the wrong order
+        # self.dataset_agent_client.set_resource({DriverParameter.RECORDS_PER_SECOND: 1})
+        # self.assert_start_sampling()
+
+        # Puts into SAMPLING mode
+        self.assert_initialize()
+
+        # Verify we get one sample
+        try:
+            # Get fours records
+            result = self.data_subscribers.get_samples(DataParticleType.INSTRUMENT, 6)
+            log.debug("First RESULT: %s", result)
+
+            # Verify values
+            self.assert_data_values(result, 'six_samples.yml')
+        except Exception as e:
+            log.error("Exception trapped: %s", e)
+            self.fail("Sample timeout.")
+
 
     def test_large_import(self):
         """
         Test importing a large number of samples from the file at once
         """
-        pass
+        self.create_sample_data('E0000002.DAT', 'E0000002.DAT')
+        self.assert_initialize()
+
+        try:
+            # Attempt to retrieve 400 samples and use a 500 second timeout
+            self.data_subscribers.get_samples(DataParticleType.INSTRUMENT, 400, 500)
+
+            # There should be 283 elements remaining in the queue
+            self.assert_sample_queue_size(DataParticleType.INSTRUMENT, 283)
+
+            # Attempt to retrieve the remaining 283 samples using a 500 second timeout
+            self.data_subscribers.get_samples(DataParticleType.INSTRUMENT, 283, 500)
+
+        except Exception as e:
+            log.error("Exception trapped: %s", e)
+            self.fail("Sample timeout.")
+
 
     def test_stop_start(self):
         """
         Test the agents ability to start data flowing, stop, then restart
         at the correct spot.
         """
-        pass
+        log.info("CONFIG: %s", self._agent_config())
+        self.create_sample_data('one.DAT', "E0000001.DAT")
+
+        self.assert_initialize()
+
+        # Verify we get one sample
+        try:
+            # Read the first file containing one record and verify the data
+            result = self.data_subscribers.get_samples(DataParticleType.INSTRUMENT, 1)
+            log.debug("RESULT 1: %s", result)
+
+            # Verify values
+            self.assert_data_values(result, 'first.yml')
+
+            # Make sure the queue is now emtpy
+            self.assert_sample_queue_size(DataParticleType.INSTRUMENT, 0)
+
+            self.create_sample_data('small.DAT', "E0000002.DAT")
+            # Now read the first four records of the second file then stop
+            result2 = self.data_subscribers.get_samples(DataParticleType.INSTRUMENT, 4)
+            log.debug("RESULT 2: %s", result2)
+            self.assert_stop_sampling()
+
+            # There should be 2 items remaining on the queue
+            self.assert_sample_queue_size(DataParticleType.INSTRUMENT, 2)
+
+            # Restart sampling and ensure we get the last 2 records of the file
+            self.assert_start_sampling()
+            result3 = self.data_subscribers.get_samples(DataParticleType.INSTRUMENT, 2)
+            log.debug("RESULT 3: %s", result3)
+            result.extend(result3)
+            self.assert_data_values(result3, 'two_samples.yml')
+
+            # There should be 0 elements in the queue
+            self.assert_sample_queue_size(DataParticleType.INSTRUMENT, 0)
+
+        except SampleTimeout as e:
+            log.error("Exception trapped: %s", e, exc_info=True)
+            self.fail("Sample timeout.")
+
 
     def test_shutdown_restart(self):
         """
         Test a full stop of the dataset agent, then restart the agent
         and confirm it restarts at the correct spot.
         """
-        pass
+        log.info("CONFIG: %s", self._agent_config())
+        self.create_sample_data('one.DAT', "E0000001.DAT")
+
+        self.assert_initialize()
+
+        # Verify we get one sample
+        try:
+            # Read the first file containing one record and verify the data
+            result = self.data_subscribers.get_samples(DataParticleType.INSTRUMENT, 1)
+            log.debug("RESULT 1: %s", result)
+
+            # Verify values
+            self.assert_data_values(result, 'first.yml')
+
+            # Make sure the queue is now emtpy
+            self.assert_sample_queue_size(DataParticleType.INSTRUMENT, 0)
+
+            self.create_sample_data('small.DAT', "E0000002.DAT")
+            # Now read the first four records of the second file then stop
+            result2 = self.data_subscribers.get_samples(DataParticleType.INSTRUMENT, 4)
+            log.debug("RESULT 2: %s", result2)
+            self.assert_stop_sampling()
+
+            # There should be 2 items remaining on the queue
+            self.assert_sample_queue_size(DataParticleType.INSTRUMENT, 2)
+
+            # stop the agent
+            self.stop_dataset_agent_client()
+            # re-start the agent
+            self.init_dataset_agent_client()
+            #re-initialize
+            self.assert_initialize(final_state=ResourceAgentState.COMMAND)
+            # Restart sampling and ensure we get the last 2 records of the file
+            self.assert_start_sampling()
+
+            result3 = self.data_subscribers.get_samples(DataParticleType.INSTRUMENT, 2)
+            log.debug("RESULT 3: %s", result3)
+            result.extend(result3)
+            self.assert_data_values(result3, 'two_samples.yml')
+
+            # There should be 0 elements in the queue
+            self.assert_sample_queue_size(DataParticleType.INSTRUMENT, 0)
+
+        except SampleTimeout as e:
+            log.error("Exception trapped: %s", e, exc_info=True)
+            self.fail("Sample timeout.")
 
     def test_parser_exception(self):
         """
         Test an exception is raised after the driver is started during
         record parsing.
         """
-        pass
+        self.clear_sample_data()
+        self.create_sample_data('E0000001-BAD-DATA.DAT', 'E0000001.DAT')
+        self.create_sample_data('one.DAT', 'E0000002.DAT')
+
+        self.assert_initialize()
+
+        self.event_subscribers.clear_events()
+        result = self.get_samples(DataParticleType.INSTRUMENT, 1)
+
+        self.assert_data_values(result, 'first.yml')
+
+        # There should be 0 elements in the queue
+        self.assert_sample_queue_size(DataParticleType.INSTRUMENT, 0)
+
+        # Verify an event was raised and we are in our retry state
+        self.assert_event_received(ResourceAgentErrorEvent, 10)
+        self.assert_state_change(ResourceAgentState.STREAMING, 10)
+
 
