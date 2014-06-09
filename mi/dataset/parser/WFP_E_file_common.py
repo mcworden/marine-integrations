291517cb828eb6ad7e62e02be391b17a59c44ea2
Mark Worden <mark_c_worden@raytheon.com>
2014-06-09 13:08:09 -0700

    Moved the regex and matchers into common.

--- mi/dataset/parser/WFP_E_file_common.py
+++ mi/dataset/parser/WFP_E_file_common.py
@@ -39,6 +39,19 @@ PROFILE_WITH_DECIM_FACTOR_MATCHER = re.compile(PROFILE_WITH_DECIM_FACTOR_REGEX)
 WFP_E_COASTAL_FLAGS_HEADER_REGEX = b'(\x00\x01\x00{7}\x01\x00\x01\x00{4})([\x00-\xff]{8})'
 WFP_E_COASTAL_FLAGS_HEADER_MATCHER = re.compile(WFP_E_COASTAL_FLAGS_HEADER_REGEX)
 
+# This regex will be used to match the flags for the global wfp e engineering record:
+# 0001 0000 0000 0001 0000 0000 0000 0001 (regex: \x00\x01\x00{5}\x01\x00{7}\x01)
+# followed by 8 bytes of variable timestamp data (regex: [\x00-\xff]{8})
+WFP_E_GLOBAL_FLAGS_HEADER_REGEX = b'(\x00\x01\x00{5}\x01\x00{7}\x01)([\x00-\xff]{8})'
+WFP_E_GLOBAL_FLAGS_HEADER_MATCHER = re.compile(WFP_E_GLOBAL_FLAGS_HEADER_REGEX)
+
+# Includes indicator/timestamp and the data consists of variable 26 bytes
+WFP_E_GLOBAL_RECOVERED_ENG_DATA_SAMPLE_REGEX = b'([\x00-\xff]{4})([\x00-\xff]{26})'
+WFP_E_GLOBAL_RECOVERED_ENG_DATA_SAMPLE_MATCHER = re.compile(WFP_E_GLOBAL_RECOVERED_ENG_DATA_SAMPLE_REGEX)
+
+# 4 bytes for the Engineering Data Record time stamp, 26 bytes for the global Engineering Data Record
+WFP_E_GLOBAL_RECOVERED_ENG_DATA_SAMPLE_BYTES = 30
+
 HEADER_BYTES = 24
 
 SAMPLE_BYTES = 26
